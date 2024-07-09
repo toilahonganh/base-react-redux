b06f98a13bbdd730b6f1b9120e4e18cd72f3f15d
Hong Anh Dinh <dinhhonganh.hcm@gmail.com>
2024-07-09 17:18:14 +0700

    How to use image in React

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./DisplayInfor.scss";
+import logo from "./../logo.svg";
 
 
 class DisplayInfor extends React.Component {
@@ -17,6 +18,8 @@ class DisplayInfor extends React.Component {
         const { listUser } = this.props;
         return (
             <div className='display-infor-container'>
+                <img src={logo} alt="Logo" />
+
                 <div>
                     <p className='span-color ' onClick={() => { this.handleShowHide() }}> {this.state.isShowListUser === true ? "Hide list user" : "Show list user"} </p>
                 </div>
